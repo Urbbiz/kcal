91e595b2bf87e080b202f68b8e78e415b2c8ae21
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-04-30 13:16:10 +0300

    ui-methods.js added

---
+++ js/components/ui-methods.js
@@ -0,0 +1,9 @@
+function addMeal() {
+    const params = {
+        name: document.getElementById("new-name").value,
+        carb: document.getElementById("new-carb").value,
+        protein: document.getElementById("new-protein").value,
+        fat: document.getElementById("new-fat").value,
+        kcal: document.getElementById("new-kcal").value,
+    };
+}
