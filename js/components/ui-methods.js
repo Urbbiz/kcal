729315b8c753b5c899921010cef94399a58085e3
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-04-30 18:37:32 +0300

    method show meals added

--- js/components/ui-methods.js
+++ js/components/ui-methods.js
@@ -1,3 +1,35 @@
+let sortType = "";
+
+function showMeals() {
+
+    let html = "";
+    const meals = diary;
+    if (meals.length) {
+        meals.forEach(function(meal) {
+
+            html += `
+            <div class="col-4 col-sm-6 col-xs-12">
+                <div class="block">
+                     <h3>${meal.name} </h3>
+                    <p> ${meal.carb} g. </p>
+                    <p> ${meal.protein} g. </p>
+                    <p> ${meal.fat} g. </p>
+                    <p> ${meal.kcal} kcal. </p>
+                
+                    <button class="btn-red" type="button" onclick="editMeal('${meal.insertDate}')">Edit</button>
+                <button class="btn-red" type="button" onclick="deleteMeal('${meal.insertDate}')">Delete</button>
+                </div>
+            </div>`;
+        });
+    } else {
+        html = '<div class="col-12">No pizzas</div>';
+    }
+    document.getElementById("meal-diary").innerHTML = html;
+};
+
+
+
+
 function addMeal() {
     const params = {
         name: document.getElementById("new-name").value,
